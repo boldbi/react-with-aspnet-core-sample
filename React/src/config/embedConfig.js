8aca9eb18fd8dd5218011dfd1149ade66e6ef6fd
Nithya Gopal <nithya.gopal@syncfusion.com>
2023-04-25 12:04:10 +0530

    UMP-307615-Configured-the-EmbedProperties-by-EmbedConfig-Json-ReactWithCore

---
+++ React/src/config/embedConfig.js
@@ -0,0 +1,12 @@
+
+const settings = {
+    "ServerUrl": "http://localhost:12345/bi", 
+    "UserEmail": "",
+    "UserPassword": "",
+    "EmbedSecret": "",
+    "EmbedType": "component",
+    "Environment": "onpremise",
+    "SiteIdentifier": "site/site1"
+}
+
+export const embedSettings = settings;
