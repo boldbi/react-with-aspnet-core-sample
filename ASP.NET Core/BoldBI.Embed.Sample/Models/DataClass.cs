f9277418b466e2b0b79d52f3977924fa24ec4af9
Nithya <nithya.gopal@syncfusion.com>
2023-05-22 14:15:06 +0530

    data class some properties removed.

--- ASP.NET Core/BoldBI.Embed.Sample/Models/DataClass.cs
+++ ASP.NET Core/BoldBI.Embed.Sample/Models/DataClass.cs
@@ -15,40 +15,7 @@ namespace BoldBI.Embed.Sample.Models
         [DataMember]
         public string dashboardServerApiUrl { get; set; }
     }
-
-    public class TokenObject
-    {
-        public string Message { get; set; }
-
-        public string Status { get; set; }
-
-        public string Token { get; set; }
-    }
-
-    public class Token
-    {
-        [JsonProperty("access_token")]
-        public string AccessToken { get; set; }
-
-        [JsonProperty("token_type")]
-        public string TokenType { get; set; }
-
-        [JsonProperty("expires_in")]
-        public string ExpiresIn { get; set; }
-
-        [JsonProperty("email")]
-        public string Email { get; set; }
-
-        public string LoginResult { get; set; }
-
-        public string LoginStatusInfo { get; set; }
-
-        [JsonProperty(".issued")]
-        public string Issued { get; set; }
-
-        [JsonProperty(".expires")]
-        public string Expires { get; set; }
-    }
+    
     public class EmbedDetails
     {
         public string Environment { get; set; }
