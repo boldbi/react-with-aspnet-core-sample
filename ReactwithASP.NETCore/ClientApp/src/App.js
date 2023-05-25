b5f2eaa57498666eb4a9236b4b85167a7df9e51a
Nithya <nithya.gopal@syncfusion.com>
2023-05-25 16:18:40 +0530

    UMP-307615-Configured-the-EmbedProperties-by-EmbedConfig-Json-ReactWithCore

---
+++ ReactwithASP.NETCore/ClientApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardListing from './DashboardListing/DashboardListing';
+
+class App extends React.Component {
+render() {
+   return (
+     <div>
+     <DashboardListing/>
+     </div>
+     );
+    }
+}
+
+export default App;
