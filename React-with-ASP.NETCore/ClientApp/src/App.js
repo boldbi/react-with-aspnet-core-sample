e55553c4a4b82829799dd79a988d2c9702b0212a
anuabarna.bsf19 <anuabarna.b@syncfusion.com>
2026-02-13 12:55:43 +0530

    updated sample with embedtoken

--- React-with-ASP.NETCore/ClientApp/src/App.js
+++ React-with-ASP.NETCore/ClientApp/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import DashboardListing from './DashboardListing/DashboardListing';
+import Dashboard from './Dashboard/Dashboard';
 
 class App extends React.Component {
 render() {
    return (
      <div>
-     <DashboardListing/>
+     <Dashboard/>
      </div>
      );
     }
